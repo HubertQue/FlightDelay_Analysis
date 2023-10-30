6ee893d114c344cdbed6fcc11b3e90cc45b1a4bd
Zizheng_Que <zqa14@sfu.ca>
2023-10-29 20:06:25 -0700

    update WBAN_Converter.py : add argparse

--- Data Processing/WBAN_Converter.py
+++ Data Processing/WBAN_Converter.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import argparse
 
 
 def merge_and_process_data(wban_file, airport_file):
@@ -7,7 +8,7 @@ def merge_and_process_data(wban_file, airport_file):
     np_airport = pd.read_csv(airport_file)['ORIGIN'].unique()
     df_airport = pd.DataFrame(np_airport, columns=['ORIGIN'])
     # print(df_airport.dtype())
-    print('count:', len(df_airport))
+    print('Total ORIGIN count:', len(df_airport))
 
     # choose 'WBAN_ID' and 'CALL_SIGN' column
     df_WBAN = df_WBAN[['WBAN_ID', 'CALL_SIGN']]
@@ -37,6 +38,7 @@ def get_station_id(wban_to_wmo_csv, sign_wban_df):
     raw_df = pd.read_csv(wban_to_wmo_csv, encoding='latin1', dtype=str,skiprows=range(opening_rows))
     print('Read WBAN mapping file.')
     mapping_columns = raw_df.loc[:, ['wmo', 'wban']]  # .loc() can avoid SettingWithCopyWarning
+    mapping_columns = mapping_columns[~mapping_columns['wmo'].isna() & ~mapping_columns['wban'].isna()]
     # mapping_columns['wban'] = mapping_columns['wban'].astype(str)
     mapping_columns['wban'] = mapping_columns['wban'].str.zfill(5)
     # mapping_columns['wmo'] = mapping_columns['wmo'].astype(str)
@@ -53,14 +55,21 @@ def get_station_id(wban_to_wmo_csv, sign_wban_df):
     return id_mapping
 
 
-# sign_to_stationID (csv/json)
-# ['sign', 'stationID']
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Data processing: generate CallSign-to-StationID mapping csv.')
+    parser.add_argument('-t0', '--airport_delay', default='data/airport_delay.csv', help='airport_delay_csv')
+    parser.add_argument('-t1', '--sign_to_wban', default='data/WBAN.csv', help='sign_to_WBAN_csv')
+    parser.add_argument('-t2', '--wban_to_wmo', default='data/master-location-identifier-database-202307_standard.csv', help='WBAN_to_WMO_csv')
+    parser.add_argument('-o', '--output', default='data/id_mapping.csv', help='output file name(csv)')
 
+    args = parser.parse_args()
+    airport_delay_csv = args.airport_delay
+    sign_to_WBAN_csv = args.sign_to_wban
+    WBAN_to_WMO_csv = args.wban_to_wmo
+    output_mapping_csv = args.output
 
-airport_delay_csv = 'data/airport_delay.csv'
-sign_to_WBAN_csv = 'data/WBAN.csv'
-WBAN_to_WMO_csv = 'data/master-location-identifier-database-202307_standard.csv'
-output_mapper_csv = 'airport_delay_final.csv'
+    sign_WBAN_df = merge_and_process_data(sign_to_WBAN_csv, airport_delay_csv)
+    sign_stationID_df = get_station_id(WBAN_to_WMO_csv, sign_WBAN_df)
 
-sign_WBAN_df = merge_and_process_data(sign_to_WBAN_csv, airport_delay_csv)
-sign_stationID_df = get_station_id(WBAN_to_WMO_csv, sign_WBAN_df)
+    sign_stationID_df.to_csv(output_mapping_csv, mode='w', index=False)
+    # columns: 'CALL_SIGN', 'stationID'
