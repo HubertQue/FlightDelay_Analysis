91dcf1c6bcdccc562b6ad11b15afff9c344e3239
sca434  <sca434@sfu.ca>
2023-11-12 15:27:59 -0800

    remove some attributes and split columns

--- Data Processing/Weather_Data_Merge_Clean.py
+++ Data Processing/Weather_Data_Merge_Clean.py
@@ -2,6 +2,47 @@ import os
 import pandas as pd
 import argparse
 
+
+def replace_by_none(data_frame, attribute_name, value):
+    data_frame[attribute_name] = data_frame[attribute_name].replace(value, 0)
+    return data_frame
+
+'''def split_columns(FRSHTT_code):
+    FRSHTT_str = str(FRSHTT_code)
+    return_list = []
+    if FRSHTT_str[0] == '0':      #if the first digit is 0, there is no record for the specific weather type
+        for i in range(7):
+            return_list.append(None)
+    elif FRSHTT_str[0] == '1':
+            return_list.append('1')
+            for i in range(len(FRSHTT_str)):
+                return_list.append(FRSHTT_str[i])
+            for j in range(6 - len(FRSHTT_str)):  # there are 7 columns to split, python stars with 0. the first one is if we have the weather records,
+                return_list.append(None)
+    else:
+        print(FRSHTT_str)
+        print('Special values in FRSHTT_str')
+    return pd.Series(return_list)'''
+
+
+def split_columns(FRSHTT_code):
+    FRSHTT_str = str(FRSHTT_code)
+    return_list = []
+    if FRSHTT_str[0] == '0':      
+        for i in range(6):
+            return_list.append('0')
+    elif FRSHTT_str[0] == '1':
+            #return_list.append('1')
+            for i in range(len(FRSHTT_str)):
+                return_list.append(FRSHTT_str[i])
+            for j in range(6 - len(FRSHTT_str)):  # there are 6 columns to split, 
+                return_list.append(0)
+    else:
+        print(FRSHTT_str)
+        print('Special values in FRSHTT_str')
+    return pd.Series(return_list)
+
+
 parser = argparse.ArgumentParser(description='Data processing: Weather Data Merge & Clean')
 parser.add_argument('-i', '--input_dir', default='E:\\SFU\\classes\\CMPT-732\\Project\\get_data', help='input file dir')
 parser.add_argument('-o', '--output_dir', default='E:\\SFU\\classes\\CMPT-732\\Project\\get_data', help='output file dir')
@@ -20,18 +61,28 @@ for file in files:
         df_list.append(pd.read_csv(file_path))
 
 df_merged = pd.concat(df_list, ignore_index=True)
-df_merged = df_merged.drop('LATITUDE', axis=1)
-df_merged = df_merged.drop('LONGITUDE', axis=1)
-df_merged = df_merged.drop('TEMP_ATTRIBUTES', axis=1)
-df_merged = df_merged.drop('DEWP_ATTRIBUTES', axis=1)
-df_merged = df_merged.drop('SLP', axis=1)
-df_merged = df_merged.drop('SLP_ATTRIBUTES', axis=1)
-df_merged = df_merged.drop('STP', axis=1)
-df_merged = df_merged.drop('STP_ATTRIBUTES', axis=1)
-df_merged = df_merged.drop('VISIB_ATTRIBUTES', axis=1)
-df_merged = df_merged.drop('WDSP_ATTRIBUTES', axis=1)
-df_merged = df_merged.drop('GUST', axis=1)
-df_merged = df_merged.drop('MAX_ATTRIBUTES', axis=1)
-df_merged = df_merged.drop('MIN_ATTRIBUTES', axis=1)
-df_merged = df_merged.drop('PRCP_ATTRIBUTES', axis=1)
+
+required_columns = ['STATION', 'DATE', 'ELEVATION', 'NAME', 'TEMP', 'DEWP', 'VISIB', 'WDSP', 'MXSPD', 'MAX', 'MIN', 'PRCP', 'SNDP', 'FRSHTT']
+optional_columns = ['LATITUDE', 'LONGITUDE', 'TEMP_ATTRIBUTES', 'DEWP_ATTRIBUTES', 'SLP_ATTRIBUTES', 'STP_ATTRIBUTES', 'VISIB_ATTRIBUTES', 'WDSP_ATTRIBUTES', 'GUST', 'MAX_ATTRIBUTES', 'MIN_ATTRIBUTES', 'PRCP_ATTRIBUTES']
+excluded_columns = ['SLP', 'STP',]
+
+#delete the optional_columns
+for i in range(len(optional_columns)):
+    df_merged = df_merged.drop(optional_columns[i], axis=1)
+
+#delete the excluded_columns
+for i in range(len(excluded_columns)):
+    df_merged = df_merged.drop(excluded_columns[i], axis=1)
+
+#------------clean data------------#
+
+##------------replace with None------------##
+replace_list = [['TEMP', 9999.9], ['DEWP', 9999.9], ['VISIB', 999.9], ['WDSP', 999.9], ['MXSPD', 999.9], ['MAX', 999.9], ['MIN', 999.9], ['PRCP', 99.99], ['SNDP', 999.9]]
+
+for i in range(len(replace_list)):
+    df_merged = replace_by_none(df_merged, replace_list[i][0], replace_list[i][1])
+
+##------------Split FRSHTT to 6 columns ------------##
+df_merged[['Fog', 'Rain or Drizzle', 'Snow or Ice Pellets', 'Hail','Thunder','Tornado or Funnel Cloud']] = df_merged['FRSHTT'].apply(split_columns)
+
 df_merged.to_csv(output_file, index=False)
