1b17f78f37424e099782de95280e9fa33ec36226
zqa14 <zqa14@sfu.ca>
2023-11-30 17:16:27 -0800

    fix data processing issue

--- Prediction/MLP.py
+++ Prediction/MLP.py
@@ -4,16 +4,20 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, classification_report, f1_score
+from sklearn.metrics import accuracy_score, classification_report, f1_score, matthews_corrcoef
 from tqdm import tqdm
+import joblib
 
 # 设置你的CSV文件路径
 file_path = '.\\airport_weather_2020.csv'
 df = pd.read_csv(file_path)
 print("Initial DataFrame shape:", df.shape)
 
+df.loc[df['CANCELLED'] == 1 , 'DEP_DELAY'] = 999
+df.loc[df['CANCELLED'] == 1 , 'DEP_TIME'] = '0:00:00 AM'
+
 # 删除不需要的列
-columns_to_drop = ['YEAR', 'FL_DATE', 'ORIGIN', 'ORIGIN_CITY_NAME', 'ORIGIN_STATE_NM', 'DEST', 'DEST_CITY_NAME', 'DEST_STATE_NM', 'CANCELLED', 'STATION',
+columns_to_drop = ['YEAR', 'FL_DATE', 'ORIGIN', 'ORIGIN_CITY_NAME', 'ORIGIN_STATE_NM', 'DEST', 'DEST_CITY_NAME', 'DEST_STATE_NM', 'STATION',
                    'DATE', 'NAME', 'FRSHTT', 'WBAN_ID', 'CALL_SIGN']
 df.drop(columns_to_drop, axis=1, inplace=True)
 print("DataFrame after dropping columns:", df.shape)
@@ -24,7 +28,9 @@ print("DataFrame after dropping NA:", df.shape)
 
 # 将小时转换为时间
 df['DEP_HOUR'] = df['DEP_TIME'].str[:2]
+df.loc[df['CANCELLED'] == 1 , 'DEP_HOUR'] = '25'
 df.drop('DEP_TIME', axis=1, inplace=True)
+df.drop('CANCELLED', axis=1, inplace=True)
 df.reset_index(drop=True, inplace=True)
 
 # 独热编码
@@ -67,12 +73,12 @@ scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-epochs = 10  # 定义迭代次数
+epochs = 5  # 定义迭代次数
 batch_size = 256  # 批量大小
-hidden_layer_size = (100, 100)
+hidden_layer_size = (50, 50)
 
 # 创建 MLP 模型
-mlp = MLPClassifier(hidden_layer_sizes=hidden_layer_size, batch_size=batch_size, max_iter=1, warm_start=True, random_state=42, class_weights='balanced')
+mlp = MLPClassifier(hidden_layer_sizes=hidden_layer_size, batch_size=batch_size, max_iter=1, warm_start=True, random_state=42)
 
 with tqdm(total=epochs, desc=f'Processing', unit='iteration') as pbar:
     for epoch in range(epochs):
@@ -85,6 +91,9 @@ y_pred = mlp.predict(X_test_scaled)
 # 评估模型
 accuracy = accuracy_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred, average='weighted')
+# 计算 Matthews 相关系数
+mcc = matthews_corrcoef(y_test, y_pred)
 print("Accuracy:", accuracy)
 print("F1 Score:", f1)
+print(f"Matthews Correlation Coefficient: {mcc}")
 print("\nClassification Report:\n", classification_report(y_test, y_pred))
